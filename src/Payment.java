8f0dda91f0227e3951863daec414ac39362dcc9e
arav <aravda2006@gmail.com>
2026-05-06 03:45:37 +0530

    Update Payment.java

--- src/Payment.java
+++ src/Payment.java
@@ -5,9 +5,8 @@ public class Payment {
     private String studentId;
     private double amount;
     private String status;
-    private String dateTime;   
-
-    public Payment(String paymentId, String studentId, double amount, String status,) {
+    
+    public Payment(String paymentId, String studentId, double amount, String status) {
         this.paymentId = paymentId;
         this.studentId = studentId;
         this.amount = amount;
@@ -29,9 +28,6 @@ public class Payment {
 
     public String getStatus() {
         return status;
-    }
-
-
     }
 
     public String toFileLine() {
